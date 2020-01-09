f5a3eec426d81571f0037f21861e84812cc163c5
elifdalkiran <elifsu6501@gmail.com>
2020-01-09 13:31:28 +0300

    düzenlemeler

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { finalize, takeUntil } from "rxjs/operators";
+import { takeUntil } from "rxjs/operators";
 import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireStorage } from '@angular/fire/storage';
 
-export interface Item{
-    title:string;
-    description:string;
-    tag:string;
-    date:Date
+export interface Item {
+    title: string;
+    description: string;
+    tag: string;
+    date: Date
 }
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -20,26 +20,28 @@ export class CardComponent implements OnInit, OnDestroy {
 
   loading: boolean = true;
   itemCollection: Observable<any[]>;
-  items:Item[]=[];
+  items:Item[] = [];
+  firstItem: Item;
 
   private unsubscribe$ = new Subject<void>();
 
-  constructor(private db: AngularFirestore, private storage: AngularFireStorage) {}
+  constructor(private db: AngularFirestore) {}
   
   ngOnInit(): void {
     this.itemCollection = this.db.collection('items').valueChanges();
     
-
     this.itemCollection
     .pipe(
       takeUntil(this.unsubscribe$)
     )
     .subscribe(data => {
-      this.items = data;
-      console.log('zxfg');
-    })
+      this.items = data.slice(1);
+      this.firstItem = data[0];
+      // console.log('zxfg');
+    });
   }
-  ngOnDestroy():void{
+
+  ngOnDestroy(): void{
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
   }
